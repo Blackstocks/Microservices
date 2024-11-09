0875e9b9a0abc957ae8e1c875ef53ea6ecf05b94
Ankit Kumar Soni <blackstocks@Ankits-MacBook-Air.local>
2024-11-10 05:02:00 +0530

    Endpoint of microservices created

---
+++ social_media_platform/core/apps/posts/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [# Add paths for posts-related views here
+]
--- social_media_platform/core/apps/users/views.py
+++ social_media_platform/core/apps/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        data = {
+            "username": user.username,
+            "email": user.email,
+        }
+        return Response(data)
